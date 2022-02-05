20b292aeafc3b75a5d46454ca53069e4ba31d214
AshmitaRakshit <89204019+AshmitaRakshit@users.noreply.github.com>
2022-02-05 16:59:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+#this is the flask api 
+#flask api is a used to make web applications using python
+from flask import Flask, jsonify, request
+from data import data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "data": data,
+        "message": "success"
+    }), 200
+
+@app.route("/planet")
+def planet():
+    name = request.args.get("name")
+    planet_data = next(item for item in data if item["name"] == name)
+    return jsonify({
+        "data": planet_data,
+        "message": "success"
+    }), 200
+    
+if __name__ == "__main__":
+  app.run()
